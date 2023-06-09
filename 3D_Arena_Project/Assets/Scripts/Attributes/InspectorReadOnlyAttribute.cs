9600aba2abb32456500b823e992e35bc789542b0
Sergey, Butko <s.butko@itransition.com>
2023-06-09 13:27:53 +0300

    Added project settings, plugins, some resources and base scene

---
+++ 3D_Arena_Project/Assets/Scripts/Attributes/InspectorReadOnlyAttribute.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Attributes
+{
+    /// <summary>
+    /// Attribute for readonly property in inspector.
+    /// </summary>
+    public class InspectorReadOnlyAttribute : PropertyAttribute
+    {
+    }
+}
